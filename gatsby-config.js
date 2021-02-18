45b4722e20f1e36ff1c33c92b1575b3b655db08b
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2021-02-18 20:55:54 +0000

    GATSBY IS HERE

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Eduardo Espadeiro",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
