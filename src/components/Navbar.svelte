945f800faccee5b28f47da4a95a73a726ea8e60f
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-07-12 13:43:00 +0100

    Improved layout

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,27 +1,42 @@
-<script>
+<script lang="ts">
     let active = false;
 
     function toggle() {
         active = !active
     }
+
+    const links = [
+      {
+        name: "About",
+        href: "/about/",
+      },
+      {
+        name: "Projects",
+        href: "/projects/",
+      },
+      {
+        name: "Blog",
+        href: "/blog/",
+      },
+    ];
 </script>
 
 <nav class="navbar is-black">
   <div class="navbar-brand">
     <a class="navbar-item has-text-weight-bold" href="/"> Eduardo Espadeiro </a>
 
-    <button class="navbar-burger { active ? 'is-active' : '' }" aria-label="menu" aria-expanded="false" on:click={ toggle }>
+    <button class="navbar-burger { active ? "is-active" : "" }" aria-label="menu" aria-expanded="false" on:click={ toggle }>
       <span aria-hidden="true" />
       <span aria-hidden="true" />
       <span aria-hidden="true" />
     </button>
   </div>
 
-  <div class="navbar-menu { active ? 'is-active' : '' }">
+  <div class="navbar-menu { active ? "is-active" : "" }">
     <div class="navbar-end">
-      <a class="navbar-item" href="/">Home</a>
-      <a class="navbar-item" href="/">Project</a>
-      <a class="navbar-item" href="/">Blog</a>
+      {#each links as link}
+        <a class="navbar-item" href={ link.href }>{ link.name }</a>
+      {/each}
     </div>
   </div>
 </nav>
