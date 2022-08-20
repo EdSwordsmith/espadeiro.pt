2f8f2d55bbedbbdb97dbb12905b48de1e709224e
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-08-20 23:47:35 +0100

    sitemap and changed dates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://espadeiro.pt",
-  integrations: [svelte()],
+  integrations: [svelte(), sitemap()]
 });
