80c5c66f088d508e1134afdebb7eae25685b984c
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-11-12 14:00:25 +0000

    refactor: nuke all files for v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
-
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://espadeiro.pt",
-  integrations: [svelte(), sitemap()]
-});
+export default defineConfig({});
