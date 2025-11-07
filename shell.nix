08b8ebf8cdd16a24430becf5550e05f8723a6c57
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2025-11-07 17:49:08 +0000

    chore: switch to shell.nix and add rust-cache

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    djlint
+    cargo
+    rustc
+    rustfmt
+    clippy
+    tailwindcss
+    openssl
+    pkg-config
+  ];
+}
